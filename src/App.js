92076768a0b69f4797329ef26007e224d7a1c8e4
MH Jafory <mhjafory@MHs-MacBook-Pro.local>
2020-04-09 13:13:31 -0400

    added add and delete button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class TodoList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+      currentTodo: "",
+    }
+  };
+
+  //this is where our methods will go
+
+  handleChange = (event) => {
+    console.log(event.target.value);
+    this.setState({
+      currentTodo: event.target.value,
+    })
+  }
+
+  addItem = (event) => {
+    event.preventDefault();
+    console.log("the add item method executed");
+    this.state.todos.push(this.state.currentTodo)
+    this.setState({
+      todos: this.state.todos,
+    })
+  }
+
+
+
+  deleteItem = () => {
+    var index = this.state.todos.indexOf(this.state.currentTodo)
+    this.state.todos.splice(index, 1)
+    this.setState({
+      todos: this.state.todos,
+    })
+  }
+
+  render() {
+    return (
+      <div className="container">
+      <h1 ClassName="title"> To do list </h1>
+
+        <form onSubmit={this.addItem}>
+          <label htmlFor="taskName">Task name:&nbsp;&nbsp;</label>
+          <input onChange={this.handleChange} name="taskName" placeholder="Place your to do here" type="text"/>
+          <button type="submit" >Add Task</button>
+        </form>
+        <ol>
+          {this.state.todos.map((value, index) => {
+            return  <li key={index}>{value}<button onClick={this.deleteItem}>Delete</button></li>
+          })}
+        </ol>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default TodoList;
